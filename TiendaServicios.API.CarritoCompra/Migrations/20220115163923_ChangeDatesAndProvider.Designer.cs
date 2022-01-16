5726248c3c5f57efe3a1e03fa2a7f674c5b0f1ab
Federico Matos <federicomatos@icloud.com>
2022-01-15 20:22:54 -0400

    feature: cambio de provider de mysql

--- TiendaServicios.API.CarritoCompra/Migrations/20220115163923_ChangeDatesAndProvider.Designer.cs
+++ TiendaServicios.API.CarritoCompra/Migrations/20220115163923_ChangeDatesAndProvider.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TiendaServicios.API.CarritoCompra.Persistencia;
 
@@ -10,9 +11,10 @@ using TiendaServicios.API.CarritoCompra.Persistencia;
 namespace TiendaServicios.API.CarritoCompra.Migrations
 {
     [DbContext(typeof(ContextoCarrito))]
-    partial class ContextoCarritoModelSnapshot : ModelSnapshot
+    [Migration("20220115163923_ChangeDatesAndProvider")]
+    partial class ChangeDatesAndProvider
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
